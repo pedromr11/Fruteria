389953a08a6df82b71b038a34d23477687bb440d
Pedro Muñoz <pedromurivas11@gmail.com>
2020-10-13 21:48:25 +0200

    Primer commit

---
+++ Fruteria_tema1/scripts.js
@@ -0,0 +1,58 @@
+let ArrayInicializar = [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0];
+let ArrayPrecio = [0.80, 1.30, 1.60, 0.90, 3, 4.50, 2.30, 0.60, 0.50, 1.40];
+let ArrayFrutas = ["Cereza", "Chirimoya", "Fresa", "Manzana", "Melon", "Naranja", "Pera", "Platano", "Sandia", "Uvas"];
+var panoja = 0;
+
+function cereza(){
+    ArrayInicializar[0] = (++ArrayInicializar[0]);
+    panoja = panoja + 0.80;
+}
+
+function chirimoya(){
+    ArrayInicializar[1] = (++ArrayInicializar[1]);
+    panoja = panoja + 1.30;
+}
+
+function fresa(){
+    ArrayInicializar[2] = (++ArrayInicializar[2]);
+    panoja = panoja + 1.60;
+}
+
+function manzana(){
+    ArrayInicializar[3] = (++ArrayInicializar[3]);
+    panoja = panoja + 0.90;
+}
+
+function melon(){
+    ArrayInicializar[4] = (++ArrayInicializar[4]);
+    panoja = panoja + 3;
+}
+
+function naranja(){
+    ArrayInicializar[5] = (++ArrayInicializar[5]);
+    panoja = panoja + 4.50;
+}
+
+function pera(){
+    ArrayInicializar[6] = (++ArrayInicializar[6]);
+    panoja = panoja + 2.30;
+}
+
+function platano(){
+    ArrayInicializar[7] = (++ArrayInicializar[7]);
+    panoja = panoja + 0.60;
+}
+
+function sandia(){
+    ArrayInicializar[8] = (++ArrayInicializar[8]);
+    panoja = panoja + 0.50;
+}
+
+function uvas(){
+    ArrayInicializar[9] = (++ArrayInicializar[9]);
+    panoja = panoja + 1.40;
+}
+
+function mostrar(){
+    
+}
