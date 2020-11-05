fdda57b66f4fb891b2f2a458bd78a22c8193d4c5
Pedro Muñoz <pedromurivas11@gmail.com>
2020-11-06 00:46:27 +0100

    Ejercicio 1 acabado

--- Fruteria_tema1/script/scripts.js
+++ Fruteria_tema1/script/scripts.js
@@ -1,63 +1,48 @@
 let ArrayKilos = [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0];
 let ArrayPrecio = [0.80, 1.30, 1.60, 0.90, 3, 4.50, 2.30, 0.60, 0.50, 1.40];
 let ArrayFrutas = ["Cereza", "Chirimoya", "Fresa", "Manzana", "Melon", "Naranja", "Pera", "Platano", "Sandia", "Uvas"];
-var panoja = 0;
+let Total = 0;
+let kgtotal = 0;
+let precioMedio = 0;
 
-function fcereza(num){
-    console.log(num);
-    console.log(ArrayPrecio[num]);
+function frutitas(num){
+  
+    ArrayKilos[num] ++;
 
-    console.log(ArrayKilos[num] ++);
 }
 
-function fchirimoya(){
-    ArrayInicializar[1] = (++ArrayInicializar[1]);
-    panoja = panoja + 1.30;
-}
-
-function ffresa(){
-    ArrayInicializar[2] = (++ArrayInicializar[2]);
-    panoja = panoja + 1.60;
-}
-
-function fmanzana(){
-    ArrayInicializar[3] = (++ArrayInicializar[3]);
-    panoja = panoja + 0.90;
-}
-
-function fmelon(){
-    ArrayInicializar[4] = (++ArrayInicializar[4]);
-    panoja = panoja + 3;
-}
-
-function fnaranja(){
-    ArrayInicializar[5] = (++ArrayInicializar[5]);
-    panoja = panoja + 4.50;
-}
-
-function fpera(){
-    ArrayInicializar[6] = (++ArrayInicializar[6]);
-    panoja = panoja + 2.30;
-}
-
-function fplatano(){
-    ArrayInicializar[7] = (++ArrayInicializar[7]);
-    panoja = panoja + 0.60;
-}
-
-function fsandia(){
-    ArrayInicializar[8] = (++ArrayInicializar[8]);
-    panoja = panoja + 0.50;
-}
-
-function fuvas(){
-    ArrayInicializar[9] = (++ArrayInicializar[9]);
-    panoja = panoja + 1.40;
-}
 
 function mostrar(){
     let textarea = document.getElementById("resultado");
-    console.log(textarea.value = "1");
-    let variable = document.createTextNode();
-    textarea.appendChild(variable);
+    
+    textarea.value = " ";
+   
+
+    for (let index = 0; index < ArrayFrutas.length; index++) {
+        
+        if(ArrayKilos[index] > 0){
+            textarea.value += "Nombre: " + ArrayFrutas[index] + " " + "Precio: " + ArrayPrecio[index] + " " + "Cantidad: " + ArrayKilos[index] + "\n";
+
+            Total += (ArrayPrecio[index] * ArrayKilos[index]);
+            kgtotal += ArrayKilos[index];
+            
+        }
+        ArrayKilos[index] = 0;
+            
+    }
+    
+    precioMedio = Total/kgtotal;  
+    
+
+    textarea.value += "Precio total: " + Total + "\n";
+    
+    textarea.value += "Precio medio: " + precioMedio;
+   
+    Total = 0;
+    kgtotal = 0
+    precioMedio = 0;
+
 }
+
+
+
